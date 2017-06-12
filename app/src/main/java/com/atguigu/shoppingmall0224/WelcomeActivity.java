ebc931c7539a02421859fad54440778395ef8350
yangguangfu520 <ygf@atguigu.com>
2017-06-12 09:50:58 +0800

    Initial commit

---
+++ app/src/main/java/com/atguigu/shoppingmall0224/WelcomeActivity.java
@@ -0,0 +1,53 @@
+package com.atguigu.shoppingmall0224;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MotionEvent;
+
+import com.atguigu.shoppingmall0224.home.activity.MainActivity;
+
+public class WelcomeActivity extends AppCompatActivity {
+
+
+    private Handler handler = new Handler();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_welcome);
+        //两秒钟进入主页面
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                //执行在主线程
+                //启动主页面
+                startMainActivity();
+            }
+        },2000);
+    }
+
+    private void startMainActivity() {
+        startActivity(new Intent(WelcomeActivity.this,MainActivity.class));
+        //关闭当前页面
+        finish();
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        //点击快速进入主页面
+        if(event.getAction()==MotionEvent.ACTION_DOWN){
+            startMainActivity();
+            return true;
+        }
+        return super.onTouchEvent(event);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        //移除消息
+        handler.removeCallbacksAndMessages(null);
+    }
+}
