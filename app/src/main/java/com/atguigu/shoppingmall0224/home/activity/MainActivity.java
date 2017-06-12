f0a624cf6595e018f1415e676a56908de06d6f7c
yangguangfu520 <ygf@atguigu.com>
2017-06-12 10:21:18 +0800

    主页面

--- app/src/main/java/com/atguigu/shoppingmall0224/home/activity/MainActivity.java
+++ app/src/main/java/com/atguigu/shoppingmall0224/home/activity/MainActivity.java
@@ -2,14 +2,25 @@ package com.atguigu.shoppingmall0224.home.activity;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.RadioGroup;
 
 import com.atguigu.shoppingmall0224.R;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
 public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.rg_main)
+    RadioGroup rgMain;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+
+        //设置默认选择首页
+        rgMain.check(R.id.rb_home);
     }
 }
