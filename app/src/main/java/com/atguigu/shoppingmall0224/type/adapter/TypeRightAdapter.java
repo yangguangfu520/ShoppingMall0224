e46060152739131e6f20a73b5504161dc8d0cdb9
yangguangfu520 <ygf@atguigu.com>
2017-06-17 10:52:01 +0800

    常用分类ViewHolder

--- app/src/main/java/com/atguigu/shoppingmall0224/type/adapter/TypeRightAdapter.java
+++ app/src/main/java/com/atguigu/shoppingmall0224/type/adapter/TypeRightAdapter.java
@@ -1,6 +1,7 @@
 package com.atguigu.shoppingmall0224.type.adapter;
 
 import android.content.Context;
+import android.content.Intent;
 import android.graphics.Color;
 import android.support.v7.widget.RecyclerView;
 import android.view.Gravity;
@@ -13,6 +14,9 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import com.atguigu.shoppingmall0224.R;
+import com.atguigu.shoppingmall0224.app.GoodsInfoActivity;
+import com.atguigu.shoppingmall0224.home.adapter.HomeAdapter;
+import com.atguigu.shoppingmall0224.home.bean.GoodsBean;
 import com.atguigu.shoppingmall0224.type.bean.TypeBean;
 import com.atguigu.shoppingmall0224.utils.Constants;
 import com.atguigu.shoppingmall0224.utils.DensityUtil;
@@ -51,6 +55,7 @@ public class TypeRightAdapter extends RecyclerView.Adapter {
     private static final int COMMON = 1;
 
 
+
     /**
      * 当前类型
      */
@@ -65,7 +70,7 @@ public class TypeRightAdapter extends RecyclerView.Adapter {
 
     @Override
     public int getItemCount() {
-        return 1;
+        return 1 + child.size();
     }
 
     @Override
@@ -83,15 +88,22 @@ public class TypeRightAdapter extends RecyclerView.Adapter {
         if (viewType == HOT) {
             View itemView = View.inflate(mContext, R.layout.item_hot_right, null);
             return new HotViewHolder(itemView);
+        } else if (viewType == COMMON) {
+            View itemView = View.inflate(mContext, R.layout.item_common_right, null);
+            return new CommonViewHolder(itemView);
         }
         return null;
     }
 
     @Override
     public void onBindViewHolder(RecyclerView.ViewHolder holder, int position) {
-        if(getItemViewType(position)==HOT){
+        if (getItemViewType(position) == HOT) {
             HotViewHolder hotViewHolder = (HotViewHolder) holder;
             hotViewHolder.setData(hot_product_list);
+        } else {
+            int realPostion = position - 1;
+            CommonViewHolder commonViewHolder = (CommonViewHolder) holder;
+            commonViewHolder.setData(child.get(realPostion));
         }
 
     }
@@ -101,18 +113,20 @@ public class TypeRightAdapter extends RecyclerView.Adapter {
         LinearLayout llHotRight;
         @BindView(R.id.hsl_hot_right)
         HorizontalScrollView hslHotRight;
+
         public HotViewHolder(View itemView) {
             super(itemView);
-            ButterKnife.bind(this,itemView);
+            ButterKnife.bind(this, itemView);
         }
 
         /**
          * 传入的数据集合
+         *
          * @param hot_product_list
          */
         public void setData(final List<TypeBean.ResultBean.HotProductListBean> hot_product_list) {
 
-            for(int i = 0; i < hot_product_list.size(); i++) {
+            for (int i = 0; i < hot_product_list.size(); i++) {
 
                 TypeBean.ResultBean.HotProductListBean listBean = hot_product_list.get(i);
 
@@ -143,7 +157,7 @@ public class TypeRightAdapter extends RecyclerView.Adapter {
 
                 //请求图片
                 Glide.with(mContext).load(Constants.BASE_URL_IMAGE + listBean.getFigure()).into(imageView);
-                myLinear.addView(imageView,ivParams);
+                myLinear.addView(imageView, ivParams);
 
 
                 //创建文本，，设置文本信息，并且添加到线性布局
@@ -156,7 +170,7 @@ public class TypeRightAdapter extends RecyclerView.Adapter {
                 textView.setTextColor(Color.parseColor("#ed3f3f"));
                 textView.setText("￥" + listBean.getCover_price());
 
-                myLinear.addView(textView,tvParams);
+                myLinear.addView(textView, tvParams);
 
 
 //
@@ -170,17 +184,61 @@ public class TypeRightAdapter extends RecyclerView.Adapter {
                 myLinear.setOnClickListener(new View.OnClickListener() {
                     @Override
                     public void onClick(View v) {
-                        int  position = (int) myLinear.getTag();
-                        TypeBean.ResultBean.HotProductListBean hotProductListBean = hot_product_list.get(position);
-                        Toast.makeText(mContext, "name="+hotProductListBean.getName()+","+hotProductListBean.getCover_price(), Toast.LENGTH_SHORT).show();
+                        int position = (int) myLinear.getTag();
+//                        TypeBean.ResultBean.HotProductListBean hotProductListBean = hot_product_list.get(position);
+//                        Toast.makeText(mContext, "name=" + hotProductListBean.getName() + "," + hotProductListBean.getCover_price(), Toast.LENGTH_SHORT).show();
+
+                        String cover_price = hot_product_list.get(position).getCover_price();
+                        String name = hot_product_list.get(position).getName();
+                        String figure = hot_product_list.get(position).getFigure();
+                        String product_id = hot_product_list.get(position).getProduct_id();
+                        GoodsBean goodsBean = new GoodsBean();
+                        goodsBean.setProduct_id(product_id);
+                        goodsBean.setFigure(figure);
+                        goodsBean.setCover_price(cover_price);
+                        goodsBean.setName(name);
+
+                        Intent intent = new Intent(mContext, GoodsInfoActivity.class);
+                        intent.putExtra(HomeAdapter.GOODS_BEAN, goodsBean);
+                        mContext.startActivity(intent);
 
                     }
                 });
 
 
-
             }
 
+        }
+    }
+
+
+    class CommonViewHolder extends RecyclerView.ViewHolder {
+        @BindView(R.id.iv_ordinary_right)
+        ImageView ivOrdinaryRight;
+        @BindView(R.id.tv_ordinary_right)
+        TextView tvOrdinaryRight;
+        @BindView(R.id.ll_root)
+        LinearLayout llRoot;
+        public CommonViewHolder(View itemView) {
+            super(itemView);
+            ButterKnife.bind(this,itemView);
+        }
+
+        public void setData(final TypeBean.ResultBean.ChildBean childBean) {
+            //设置图片
+            Glide.with(mContext).load(Constants.BASE_URL_IMAGE + childBean.getPic()).placeholder(R.drawable.new_img_loading_2)
+                    .error(R.drawable.new_img_loading_2).into(ivOrdinaryRight);
+
+            //设置名称
+            tvOrdinaryRight.setText(childBean.getName());
+
+            //设置点击事件
+            llRoot.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Toast.makeText(mContext, "name=="+childBean.getName(), Toast.LENGTH_SHORT).show();
+                }
+            });
         }
     }
 
